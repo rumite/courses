9ae7d28f0a8404682683de8cee05288e68b4c703
rumite <bakhtiyarowtn@gmail.com>
2020-02-19 21:54:10 +0300

    hw1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Rectangle r = new Rectangle(8,10, "rrr!");
+        System.out.println("Площадь прямоугольника =" + r.area());
     }
+
+    void championship() {
+        Champion champ1 = new Champion(1,2,3);
+        Champion champ2 = new Champion(4,6,7);
+        int sum = Champion.s 
+
+    }
+
+    class Cat {
+        String Name;
+        int Age;
+
+    }
+
+
 }
